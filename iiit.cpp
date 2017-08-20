0a56f7f0e2b070e94cced3c3653b9b301bef02dd
shivanktripathi <shivanktriathi344@gmail.com>
2017-08-20 12:44:55 +0530

    changes

---
+++ iiit.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int pclass[10000];
+for(iny i=0;i<10000;i++)
+{
+if(sex[i]==false)
+survived=1;
+else
+survived=0;
+cout<<survived<<endl;
+}
+}
